1d232b11195996e7f0401c801bebc0bacf827045
Annwan <anwankong@gmail.com>
2019-10-16 12:57:27 +0200

    started to work on schemes

---
+++ schemes.py
@@ -0,0 +1,22 @@
+from asciimatics.screen import Screen as S
+colorScheme = {
+    "background": (          ), # Frame background
+    "borders" # Frame border and Divider Widget
+    "button" # Buttons
+    "control"                   # Checkboxes and RadioButtons
+    "disabled"                  # Any disabled Widget
+    "edit_text"                 # Text and TextBox
+    "field"                     , # Value of an option for a Checkbox, RadioButton or Listbox
+    "focus_button"              , # Buttons with input focus
+    "focus_control"             , # Checkboxes and RadioButtons with input focus
+    "focus_edit_text"           , # Text and TextBox with input focus
+    "focus_field"               , # As above with input focus
+    "invalid"                   , # The widget contains invalid data
+    "label"                     , # Widget labels
+    "scroll"                    , # Frame scroll bar
+    "selected_control"          , # Checkboxes and RadioButtons when selected
+    "selected_field"            , # As above when selected
+    "selected_focus_control"    , # Checkboxes and RadioButtons with both
+    "selected_focus_field"      , # As above with both
+    "title"                     , # Frame title
+}
